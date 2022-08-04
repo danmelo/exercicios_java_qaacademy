7e678db6a7aa924e650c5b7d90190da2e63cb2fc
danmelo <danmelorayder@gmail.com>
2022-08-04 19:44:46 -0300

    Exercicium medium

---
+++ src/main/java/medium/Exercicio_01_medium.java
@@ -0,0 +1,21 @@
+package medium;
+
+public class Exercicio_01_medium {
+
+   static int i;
+
+    public static void main(String[] args) {
+
+        for(i=1; i<=100; i++){
+
+            if (i%2==0){
+                System.out.println(i + " é PAR");
+            }else {
+                System.out.println(i + " é ÍMPAR");
+            }
+
+        }
+
+    }
+
+}
